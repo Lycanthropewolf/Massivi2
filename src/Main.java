b1302c556516383e6de7adc06967b4c81f1b5a42
Антон <antonolegovich2017@icloud.com>
2022-08-08 20:11:22 +0600

    Массивы 2 домашка

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,7 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-
-    }
-
-
-    int[] days = generateRandomArray();
-
-
-    public static int[] generateRandomArray() {
-        java.util.Random random = new java.util.Random();
-        int[] days = new int[30];
-        for (int i = 0; i < days.length; i++) {
-            days[i] = random.nextInt(100_000) + 100_000;
-        }
+        int[] days = generateRandomArray();
 
 
         //Задание 1
@@ -30,31 +18,37 @@ public class Main {
         for (int i = 0; i < days.length; i++) {
             if (days[i] > maxPayment) {
                 maxPayment = days[i];
-            }
-            if (days[i] < minPayment) {
+            } else if (days[i] < minPayment) {
                 minPayment = days[i];
             }
-        }
-        System.out.println("Минимальная сумма затрат составила " + maxPayment + " рублей" + " Максимальная сумма затрат составила " + minPayment + " рублей.");
-
+        }System.out.println("Минимальная сумма затрат составила " + minPayment + " рублей" + " Максимальная сумма затрат составила " + maxPayment + " рублей.");
+        System.out.println();
 
         // Задание 3
-        for (int i = 0; i < days.length; i++) {
-            sum += days[i];
-            double averagePaymentAmount = sum / 30;
+
+            double averagePaymentAmount = (double)sum / days.length;
             System.out.println(averagePaymentAmount);
-        }
 
 
         // Задание 4
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length - 1; i >= 0; i--) {
-            System.out.print(reverseFullName[i]);
+        for (int m = reverseFullName.length - 1; m >= 0; m--) {
+            System.out.print(reverseFullName[m]);
         }
-
-
     }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] days = new int[30];
+        for (int i = 0; i < days.length; i++) {
+            days[i] = random.nextInt(100_000) + 100_000;
+        }
+        return days;
+    }
+
 }
+
+
 
 
 
